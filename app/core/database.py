78963b8619c685069981b4b86333ff5610fc85d5
Huy-8bit <huy8bit@gmail.com>
2024-04-20 08:17:52 +0700

    update

--- app/core/database.py
+++ app/core/database.py
@@ -1,23 +1,20 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import settings
 
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-ip = os.getenv("MONGO_IP")
-
-MONGO_INITDB_ROOT_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
-MONGO_INITDB_ROOT_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
-MONGO_HOST = os.getenv("MONGO_IP")
+# MONGO_INITDB_ROOT_USERNAME = ""
+# MONGO_INITDB_ROOT_PASSWORD = ""
+MONGO_HOST = "13.212.87.149"
 MONGO_PORT = "27017"
 MONGO_DATABASE = "cloud_backup"
 
-DATABASE_URL = f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DATABASE}?authSource=admin"
+# DATABASE_URL = f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DATABASE}?authSource=admin"
 
+DATABASE_URL = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DATABASE}"
+
+# DATABASE_URL = "mongodb://54.254.58.42:27017"
 
 client = AsyncIOMotorClient(DATABASE_URL)
 
 # Define the MongoDB database
-database = client["project1"]
+database = client["cloud_backup"]
